aeddb5d559f541b50ecc28fd90771d8ef35c9b5d
unknown <shalomsonekar.1@gmail.com>
2026-03-16 16:45:16 +0530

    basename added to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,10 @@ export default function App() {
       <UserProvider>
         <ScoreProvider>
           <UIProvider>
-            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <BrowserRouter
+              basename="/dbs-app"
+              future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+            >
               <AppRoutes />
               <Modal />
             </BrowserRouter>
